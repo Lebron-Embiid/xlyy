74c6c03b0cce217bd5bce5c4c071fc6d3d2b9309
lebron-embiid <973265025@qq.com>
2019-05-25 08:58:31 +0800

    忘记密码

--- forget_pwd.php
+++ forget_pwd.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>账号密码登录</title>
+	<title>忘记密码</title>
 	<meta name="keywords" content="关键词">
 	<meta name="description" content="网站描述">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no" />
@@ -15,17 +15,34 @@
 	<script src="js/rem.js"></script>
 	<script src="js/jquery-2.1.4.min.js"></script>
 	<script src="js/bootstrap.min.js"></script>
+	<script src="js/code.js"></script>
 	<script src="js/common.js"></script>
+	<style>
+		.login_form_box .container{
+			padding-bottom: .8rem;
+		}
+		.forget{
+			margin-bottom: 0;
+		}
+		.login_btn{
+			margin-top: 1rem;
+		}
+	</style>
 </head>
 <body>
 	<?php include 'header_login.php'; ?>
 	<div class="login_form_box">
 		<div class="container">
-			<form action="vip_index.php" method="post">
+			<form action="" method="post">
 				<div class="input_box white">
 					<img src="images/form_icon2.png" class="left_icon" alt="">
 					<input type="text" class="input show_text phone_txt" placeholder="手机号码" name="phone">
 					<img src="images/close.png" class="right_icon right_close" alt="">
+				</div>
+				<div class="input_box white">
+					<img src="images/form_icon3.png" class="left_icon" alt="">
+					<input type="text" placeholder="验证码" name="code">
+					<input type="button" value="获取验证码" class="getCode"></input>
 				</div>
 				<div class="input_box white">
 					<img src="images/form_icon4.png" class="left_icon" alt="">
@@ -33,13 +50,8 @@
 					<img src="images/eye.png" class="right_eye right_eye1 active" alt="">
 					<img src="images/eye1.png" class="right_eye right_eye2" alt="">
 				</div>
-				<div class="remember_box">
-					<label for="remember"><input type="checkbox" name="remember" id="remember">记住我</label>
-					<a href="register.php?sex=0" style="color: #777;"><span>新用户注册</span></a>
-				</div>
-				<input type="submit" value="登录" class="login_btn">
-				<p class="forget"><a href="forget_pwd.php">忘记密码</a><a href="login_phone.php">短信验证码登录</a></p>
-				<!-- <p class="new"><a href="register.php?sex=0">新用户注册</a><span>|</span><a href="">社交帐号登录</a></p> -->
+				<input type="submit" value="提交" class="login_btn">
+				<p class="forget"><a href="javascript:void(0);"></a><a href="login.php">返回登录</a></p>
 			</form>
 		</div>
 	</div>	
